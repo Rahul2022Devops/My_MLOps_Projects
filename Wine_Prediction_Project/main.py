cb405d8add6647d059b90640fb0590aea0c2f75b
Rahul Hasan <careerrahulhasan93@gmail.com>
2026-06-08 14:25:10 +0530

    code updated

--- Wine_Prediction_Project/main.py
+++ Wine_Prediction_Project/main.py
@@ -32,15 +32,16 @@ except Exception as e:
     raise e
 
 
-STAGE_NAME = "Data Transformation Stage"    
+
+STAGE_NAME = "Data Transformation stage"
 try:
-    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
-    data_transformation = DataTransformationTrainingPipeline()
-    data_transformation.main()
-    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx============================x")      
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataTransformationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
-    logger.exception(e)
-    raise e
+        logger.exception(e)
+        raise e
 
 
 STAGE_NAME = "Model Trainer Stage"
