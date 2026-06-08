6fa2075a6a1d0dee8ba7c35c4141878aca5ff8fd
Rahul Hasan <careerrahulhasan93@gmail.com>
2026-06-08 11:42:57 +0530

    code updated

--- Wine_Prediction_Project/main.py
+++ Wine_Prediction_Project/main.py
@@ -1,6 +1,8 @@
 from My_Wine_Sample_Project import logger
 from My_Wine_Sample_Project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from My_Wine_Sample_Project.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from My_Wine_Sample_Project.entity.config_entity import DataTransformationConfig
+from My_Wine_Sample_Project.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -23,6 +25,17 @@ try:
     data_validation.main()
     logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx============================x")
 
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Transformation Stage"    
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx============================x")      
 except Exception as e:
     logger.exception(e)
     raise e
--- Wine_Prediction_Project/src/My_Wine_Sample_Project/entity/config_entity.py
+++ Wine_Prediction_Project/src/My_Wine_Sample_Project/entity/config_entity.py
@@ -17,5 +17,8 @@ class DataValidationConfig:
     all_schema: dict
 
 
-
+@dataclass(frozen=True)
+class DataTransformationConfig:
+    root_dir: Path
+    data_path: Path
 
